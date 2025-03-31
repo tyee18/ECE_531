91bfcc113b14199860db0e0eb2068044a2301c12
tyee18 <tyee@arizona.edu>
2025-03-30 23:37:05 -0700

    Finalized SRRC Filter experiment with lots of redundant code.

--- Lab_5/Source_Code/Yee_Taylor_srrcFilterData_Updated.m
+++ Lab_5/Source_Code/Yee_Taylor_srrcFilterData_Updated.m
@@ -7,8 +7,6 @@ qpskMod = comm.QPSKModulator('BitInput',true);
 % Create a QPSK demodulator System object and modulate data
 qpskDeMod = comm.QPSKDemodulator('BitOutput',true);
 % Set up filters
-% Note - add 'RolloffFactor' to each filter as needed (for beta, in lab
-% equations).
 rollOffFactors_2_1 = [0, 0.1, 0.25, 0.5, 1];
 steps = 20;
 
@@ -41,7 +39,7 @@ for testFactors = 1:length(rollOffFactors_2_1)
 
     % Receive and view signal
     for k=1:steps
-        %scope(rxFilt)
+        scope(rxFilt)
     end
 end
 
@@ -71,10 +69,9 @@ for testFactors = 1:length(rollOffFactors_2_2)
         delayedsig = VFD(modFiltData, tau_hat);
         % Add noise at the rx
         rxSig = awgn(delayedsig,25); % Add noise
-        rxFilt = [rxFilt;rcrFilt(rxSig)]; % Rx filter
+        rxFilt = [rcrFilt(rxSig)]; % Rx filter
         demodNoiseData = qpskDeMod(rcrFilt(rxSig));
         demodFiltData  = qpskDeMod(rcrFilt(rxFilt));
-        %newRxFilt = rcrFilt(rxSig);
     end
 
     % For Subplot 1:
@@ -92,25 +89,8 @@ for testFactors = 1:length(rollOffFactors_2_2)
         end
     end
 
-    %{
     % For Subplot 2:
-    % Fill in the tx and rx data lost due to decimation, compared to the Rx
-    % filter output
-    newFiltDataSub2 = [];
-    ogRxFiltInd     = 1;
-
-    for subplot2_FiltInd = 1:length(demodFiltData)
-        if mod(subplot2_FiltInd, 2) == 0
-            newFiltDataSub2(subplot2_FiltInd) = nan
-            ogRxFiltInd = ogRxFiltInd + 1
-        else
-            newFiltDataSub2(subplot2_FiltInd) = newRxFilt(ogRxFiltInd)
-        end
-    end
-    %}
-
-    % For Subplot 2:
-    % Fill in the tx and rx data lost due to decimation, compared to the Rx
+    % Fill in the tx data lost due to decimation, compared to the Rx
     % filter output
     newTxDataSub2 = [];
     ogTxDataInd    = 1;
@@ -121,17 +101,7 @@ for testFactors = 1:length(rollOffFactors_2_2)
         newTxDataSub2(subplot2_txDataInd+1:1:ogTxDataInd*rxFiltTxDataRatio) = nan;
         ogTxDataInd = ogTxDataInd + 1;
     end
-%{
-    newDemodDataSub2 = [];
-    ogDemodDataInd   = 1;
-    rxFiltDemodRatio = length(rxFilt) / length(demodNoiseData);
 
-    for subplot2_demodDataInd = 1:rxFiltDemodRatio:length(rxFilt)
-        newDemodDataSub2(subplot2_demodDataInd) = demodNoiseData(ogDemodDataInd);
-        newDemodDataSub2(subplot2_demodDataInd+1:1:ogDemodDataInd*rxFiltDemodRatio) = nan;
-        ogDemodDataInd = ogDemodDataInd + 1;
-    end
-%}
     %% Subplot 1: Tx Data, Rx Data w/ Noise, Tx SRRC
     figure;
     subplot(3, 1, 1);
@@ -153,11 +123,6 @@ for testFactors = 1:length(rollOffFactors_2_2)
     stem(newTxDataSub2, '-kx');
     plot(abs(rxFilt), 'r');
     stem(demodFiltData, '-mo');
-    %{
-    stem(newTxDataSub1, '-kx');
-    plot(abs(newFiltDataSub2), 'r');
-    stem(demodFiltData, '-mo');
-    %}
     legend('Transmitted Data', 'Rcv Filter Output', 'Demodulated');
     hold off;
 
