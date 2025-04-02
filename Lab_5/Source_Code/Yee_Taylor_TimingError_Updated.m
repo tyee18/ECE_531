572280da3b16534d8483270bfa75251b1a496968
tyee18 <tyee@arizona.edu>
2025-04-01 23:06:59 -0700

    Adding some skeleton code and markers to TImingError.

--- Lab_5/Source_Code/Yee_Taylor_TimingError_Updated.m
+++ Lab_5/Source_Code/Yee_Taylor_TimingError_Updated.m
@@ -12,7 +12,11 @@ cdPost = comm.ConstellationDiagram('ReferenceConstellation', [-1 1],...
 cdPre.Position(1) = 50;
 cdPost.Position(1) = cdPre.Position(1)+cdPre.Position(3)+10;% Place side by side
 %% Impairments
-snr = 15; timingOffset = samplesPerSymbol*0.01; % Samples
+snr = 15; %% todo: update and loop through this with SNR = 5dB, 10dB, 15dB, 20dB.
+phaseOffset  = pi/8; % radians
+pfo          = comm.PhaseFrequencyOffset('PhaseOffset', rad2deg(phaseOffset), 'SampleRate', sampleRateHz);
+timingOffset = samplesPerSymbol*0.01; % Samples
+
 %% Generate symbols
 data = randi([0 modulationOrder-1], numSamples*2, 1);
 mod = comm.DBPSKModulator(); modulatedData = mod(data);
@@ -30,6 +34,10 @@ chan = comm.AWGNChannel('NoiseMethod','Signal to noise ratio (SNR)','SNR',snr, .
     'SignalPower',1,'RandomStream', 'mt19937ar with seed');
 %% Add delay
 varDelay = dsp.VariableFractionalDelay;
+
+%% Add error vector magnitude (EVM)
+evm = comm.EVM;
+
 %% Model of error
 % Add timing offset to baseband signal
 filteredData = [];
@@ -41,8 +49,15 @@ for k=1:frameSize:(numSamples - frameSize)
     noisyData = chan(filteredTXData);
     % Time delay signal
     offsetData = varDelay(noisyData, k/frameSize*timingOffset);
+    % Phase offset signal
+    phaseOffsetData = pfo(offsetData);
     % Filter signal
     filteredData = RxFlt(offsetData);filteredDataRef = RxFltRef(noisyData);
-    % Visualize Error
-    cdPre(filteredDataRef);cdPost(filteredData);pause(0.1); %#ok<*UNRCH>
+
+    %% Everything else here is new: implement TED here.
+
+    % Visualize Error - this can be commented out as needed
+    %cdPre(filteredDataRef);cdPost(filteredData);pause(0.1); %#ok<*UNRCH>
 end
+
+[testRMSEVM, testMaxEVM, pctEVM] = evm(filteredDataRef, filteredData);
