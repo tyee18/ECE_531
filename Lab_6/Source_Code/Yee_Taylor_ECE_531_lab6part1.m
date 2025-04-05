87417a9049ab1d34de36387e8f289e495dab0f8e
tyee18 <tyee@arizona.edu>
2025-04-05 14:55:19 -0700

    Fixed spacing, updated lab6part1 with modulation scheme options.

--- Lab_6/Source_Code/Yee_Taylor_ECE_531_lab6part1.m
+++ Lab_6/Source_Code/Yee_Taylor_ECE_531_lab6part1.m
@@ -7,8 +7,8 @@ samplesPerSymbol = 1;
 frameSize = 2^10;
 numFrames = 100;
 numSamples = numFrames*frameSize; % Samples to simulate
-modulationOrder = 2;
-filterUpsample = 4;
+modulationOrder  = 2;
+filterUpsample   = 4;
 filterSymbolSpan = 8;
 
 %% Impairments
@@ -18,11 +18,11 @@ phaseOffset = 0; % Radians
 
 %% Generate symbols
 data = randi([0 samplesPerSymbol], numSamples, 1);
-mod = comm.DBPSKModulator();
+mod = comm.QPSKModulator(); % modulation scheme can be updated between sections 3.2.1 and 3.2.2 as needed
 modulatedData = mod.step(data);
 
 %% Add TX Filter
-TxFlt = comm.RaisedCosineTransmitFilter('OutputSamplesPerSymbol', filterUpsample, 'FilterSpanInSymbols', filterSymbolSpan);
+TxFlt        = comm.RaisedCosineTransmitFilter('OutputSamplesPerSymbol', filterUpsample, 'FilterSpanInSymbols', filterSymbolSpan);
 filteredData = step(TxFlt, modulatedData);
 
 %% Add noise
@@ -31,6 +31,10 @@ noisyData = awgn(filteredData,snr);%,'measured');
 %% Setup visualization object(s)
 sa = dsp.SpectrumAnalyzer('SampleRate',sampleRateHz,'ShowLegend',true);
 
+%% Set up coarse frequency compensator (FFT-based by default)
+coarseFreqComp = comm.CoarseFrequencyCompensator('Modulation','QPSK'); % modulation scheme can be updated between sections 3.2.1 and 3.2.2 as needed
+ModulationType = string(coarseFreqComp.Modulation);
+
 %% Model of error
 % Add frequency offset to baseband signal
 
@@ -38,7 +42,8 @@ for offsetIncrease = sampleRateOffsetStepHz:sampleRateOffsetStepHz:sampleRateHz
     % Precalculate constant(s)
     normalizedOffset = 1i.*2*pi*(offsetIncrease)./sampleRateHz;
 
-    offsetData = zeros(size(noisyData));
+    offsetData    = zeros(size(noisyData));
+    coarseCorrectedData = zeros(size(noisyData));
     for k=1:frameSize:numSamples*filterUpsample
 
         % Create phase accurate vector
@@ -48,11 +53,17 @@ for offsetIncrease = sampleRateOffsetStepHz:sampleRateOffsetStepHz:sampleRateHz
         % Offset data and maintain phase between frames
         offsetData(timeIndex) = (noisyData(timeIndex).*freqShift);
 
+        % Correct data with coarse frequency compensator
+        coarseCorrectedData(timeIndex) = coarseFreqComp(offsetData(timeIndex));
+
         % Visualize Error - todo, this can be commented out for expediency
         %step(sa,[noisyData(timeIndex),offsetData(timeIndex)]);pause(0.1); %#ok<*UNRCH>
 
+        % Visualize Corrected Data - todo, this can be commented out for expediency
+        %step(sa,[noisyData(timeIndex),coarseCorrectedData(timeIndex)]);pause(0.1); %#ok<*UNRCH>
+
     end
-    %% Set up fig info to be saved
+    %% Section 2.1: Set up fig info to be saved
     figPlot = figure;
     figName = sprintf("MATLAB_2.1_2_offset_%d", offsetIncrease);
     df = sampleRateHz/frameSize;
@@ -64,6 +75,26 @@ for offsetIncrease = sampleRateOffsetStepHz:sampleRateOffsetStepHz:sampleRateHz
     grid on;xlabel('Frequency (Hz)');ylabel('PSD (dB)');
     legend('Original','Offset','Location','Best');
     title(sprintf('PSDs of offset and original transmitted signals with offset = %d Hz', offsetIncrease));
+    NumTicks = 5;L = h(1).Parent.XLim;
+    set(h(1).Parent,'XTick',linspace(L(1),L(2),NumTicks))
+
+    %% Save figure
+    saveas(figPlot, strcat(figName, '.fig'));
+    saveas(figPlot, strcat(figName, '.jpg'));
+
+
+    %% Section 3: Set up fig info to be saved
+    figPlot = figure;
+    figName = sprintf("MATLAB_3.2_2_offset_%d", offsetIncrease);
+    df = sampleRateHz/frameSize;
+    %% Plot
+    frequencies = -sampleRateHz/2:df:sampleRateHz/2-df;
+    spec = @(sig) fftshift(10*log10(abs(fft(sig))));
+    h = plot(frequencies, spec(noisyData(timeIndex)),...
+        frequencies, spec(coarseCorrectedData(timeIndex)));
+    grid on;xlabel('Frequency (Hz)');ylabel('PSD (dB)');
+    legend('Original','Coarse Corrected Offset','Location','Best');
+    title(sprintf('(%s) PSDs of coarse freq corrected offset and original tx signals with offset = %d Hz', ModulationType, offsetIncrease));
     NumTicks = 5;L = h(1).Parent.XLim;
     set(h(1).Parent,'XTick',linspace(L(1),L(2),NumTicks))
 
