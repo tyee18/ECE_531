2d1a9f1bbae1b4a4941f814f9e0d18c1980ef490
tyee18 <tyee@arizona.edu>
2025-04-04 18:34:27 -0700

    Working revision of lab6part1, updated TimingError for Lab5 to use MATLAB toolbox instead of homemade timing algorithm.

--- Lab_6/Source_Code/Yee_Taylor_ECE_531_lab6part1.m
+++ Lab_6/Source_Code/Yee_Taylor_ECE_531_lab6part1.m
@@ -1,5 +1,8 @@
+clear all; close all;
+
 %% General system details
 sampleRateHz = 1e6; % Sample rate
+sampleRateOffsetStepHz = 0.1*sampleRateHz; % Offset in hertz
 samplesPerSymbol = 1;
 frameSize = 2^10;
 numFrames = 100;
@@ -10,7 +13,7 @@ filterSymbolSpan = 8;
 
 %% Impairments
 snr = 15;
-frequencyOffsetHz = 1e5; % Offset in hertz
+frequencyOffsetHz     = 1e5; % Offset in hertz
 phaseOffset = 0; % Radians
 
 %% Generate symbols
@@ -31,30 +34,40 @@ sa = dsp.SpectrumAnalyzer('SampleRate',sampleRateHz,'ShowLegend',true);
 %% Model of error
 % Add frequency offset to baseband signal
 
-% Precalculate constant(s)
-normalizedOffset = 1i.*2*pi*frequencyOffsetHz./sampleRateHz;
+for offsetIncrease = sampleRateOffsetStepHz:sampleRateOffsetStepHz:sampleRateHz
+    % Precalculate constant(s)
+    normalizedOffset = 1i.*2*pi*(offsetIncrease)./sampleRateHz;
 
-offsetData = zeros(size(noisyData));
-for k=1:frameSize:numSamples*filterUpsample
+    offsetData = zeros(size(noisyData));
+    for k=1:frameSize:numSamples*filterUpsample
 
-    % Create phase accurate vector
-    timeIndex = (k:k+frameSize-1).';
-    freqShift = exp(normalizedOffset*timeIndex + phaseOffset);
-    
-    % Offset data and maintain phase between frames
-    offsetData(timeIndex) = (noisyData(timeIndex).*freqShift);
- 
-    % Visualize Error
-    step(sa,[noisyData(timeIndex),offsetData(timeIndex)]);pause(0.1); %#ok<*UNRCH>
+        % Create phase accurate vector
+        timeIndex = (k:k+frameSize-1).';
+        freqShift = exp(normalizedOffset*timeIndex + phaseOffset);
 
+        % Offset data and maintain phase between frames
+        offsetData(timeIndex) = (noisyData(timeIndex).*freqShift);
+
+        % Visualize Error - todo, this can be commented out for expediency
+        %step(sa,[noisyData(timeIndex),offsetData(timeIndex)]);pause(0.1); %#ok<*UNRCH>
+
+    end
+    %% Set up fig info to be saved
+    figPlot = figure;
+    figName = sprintf("MATLAB_2.1_2_offset_%d", offsetIncrease);
+    df = sampleRateHz/frameSize;
+    %% Plot
+    frequencies = -sampleRateHz/2:df:sampleRateHz/2-df;
+    spec = @(sig) fftshift(10*log10(abs(fft(sig))));
+    h = plot(frequencies, spec(noisyData(timeIndex)),...
+        frequencies, spec(offsetData(timeIndex)));
+    grid on;xlabel('Frequency (Hz)');ylabel('PSD (dB)');
+    legend('Original','Offset','Location','Best');
+    title(sprintf('PSDs of offset and original transmitted signals with offset = %d Hz', offsetIncrease));
+    NumTicks = 5;L = h(1).Parent.XLim;
+    set(h(1).Parent,'XTick',linspace(L(1),L(2),NumTicks))
+
+    %% Save figure
+    saveas(figPlot, strcat(figName, '.fig'));
+    saveas(figPlot, strcat(figName, '.jpg'));
 end
-%% Plot
-df = sampleRateHz/frameSize;
-frequencies = -sampleRateHz/2:df:sampleRateHz/2-df;
-spec = @(sig) fftshift(10*log10(abs(fft(sig))));
-h = plot(frequencies, spec(noisyData(timeIndex)),...
-     frequencies, spec(offsetData(timeIndex)));
-grid on;xlabel('Frequency (Hz)');ylabel('PSD (dB)');
-legend('Original','Offset','Location','Best');
-NumTicks = 5;L = h(1).Parent.XLim;
-set(h(1).Parent,'XTick',linspace(L(1),L(2),NumTicks))
