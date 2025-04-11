5199e6ac79e881358fbe4bd1ef1b7554d83e99e1
tyee18 <tyee@arizona.edu>
2025-04-10 21:57:23 -0700

    Half-working lab 6 source code.

--- Lab_6/Source_Code/Yee_Taylor_ECE_531_lab6part1_wFFC.m
+++ Lab_6/Source_Code/Yee_Taylor_ECE_531_lab6part1_wFFC.m
@@ -13,7 +13,7 @@ filterSymbolSpan = 8;
 
 %% Debugging flags
 visualOffset          = false;
-visualOffsetCorrected = false;
+visualOffsetCorrected = true;
 plot_2_1 = false;
 plot_3_2 = false;
 modType  = 'BPSK';
@@ -29,17 +29,17 @@ data = randi([0 samplesPerSymbol], numSamples, 1);
 if strcmp(modType, 'DBPSK')
     mod = comm.DBPSKModulator(); % modulation scheme can be updated between sections 3.2.1 and 3.2.2 as needed
     coarseFreqComp = comm.CoarseFrequencyCompensator('Modulation','BPSK');
-    fineFreqComp = comm.CarrierSynchronizer('Modulation','BPSK', ...
+    fineFreqComp   = comm.CarrierSynchronizer('Modulation','BPSK', ...
     'SamplesPerSymbol',samplesPerSymbol, 'CustomPhaseOffset',phaseOffset);
 elseif strcmp(modType, 'QPSK')
     mod = comm.QPSKModulator(); % modulation scheme can be updated between sections 3.2.1 and 3.2.2 as needed
     coarseFreqComp = comm.CoarseFrequencyCompensator('Modulation','QPSK');
-    fineFreqComp = comm.CarrierSynchronizer('Modulation','QPSK', ...
+    fineFreqComp   = comm.CarrierSynchronizer('Modulation','QPSK', ...
     'SamplesPerSymbol',samplesPerSymbol, 'CustomPhaseOffset',phaseOffset);
 else
     mod = comm.BPSKModulator(); % let default be BPSK
     coarseFreqComp = comm.CoarseFrequencyCompensator('Modulation','BPSK');
-    fineFreqComp = comm.CarrierSynchronizer('Modulation','BPSK', ...
+    fineFreqComp   = comm.CarrierSynchronizer('Modulation','BPSK', ...
     'SamplesPerSymbol',samplesPerSymbol, 'CustomPhaseOffset',phaseOffset);
 end
 
@@ -61,83 +61,86 @@ evm = comm.EVM('ReferenceSignalSource','Estimated from reference constellation')
 %% Model of error
 % Add frequency offset to baseband signal
 
-for offsetIncrease = sampleRateOffsetStepHz:sampleRateOffsetStepHz:sampleRateHz
-    % Precalculate constant(s)
-    normalizedOffset = 1i.*2*pi*(offsetIncrease)./sampleRateHz;
+% Precalculate constant(s)
+normalizedOffset = 1i.*2*pi*frequencyOffsetHz./sampleRateHz;
 
-    offsetData    = zeros(size(noisyData));
-    coarseCorrectedData = zeros(size(noisyData));
-    for k=1:frameSize:numSamples*filterUpsample
+offsetData          = zeros(size(noisyData));
+coarseCorrectedData = zeros(size(noisyData));
+fineCorrectedData   = zeros(size(noisyData));
+correctedPhaseEstimate   = zeros(size(noisyData));
+rmsEVM   = zeros(size(noisyData));
 
-        % Create phase accurate vector
-        timeIndex = (k:k+frameSize-1).';
-        freqShift = exp(normalizedOffset*timeIndex + phaseOffset);
+for k=1:frameSize:numSamples*filterUpsample
 
-        % Offset data and maintain phase between frames
-        offsetData(timeIndex) = (noisyData(timeIndex).*freqShift);
+    % Create phase accurate vector
+    timeIndex = (k:k+frameSize-1).';
+    freqShift = exp(normalizedOffset*timeIndex + phaseOffset);
 
-        % Correct data with coarse frequency compensator
-        coarseCorrectedData(timeIndex) = coarseFreqComp(offsetData(timeIndex));
+    % Offset data and maintain phase between frames
+    offsetData(timeIndex) = (noisyData(timeIndex).*freqShift);
 
-        % Correct data with fine frequency compensator
-        [fineCorrectedData(timeIndex), correctedPhaseEstimate(timeIndex)] = ...
-            fineFreqComp(coarseCorrectedData(timeIndex));
+    % Correct data with coarse frequency compensator
+    coarseCorrectedData(timeIndex) = coarseFreqComp(offsetData(timeIndex));
 
-        if visualOffset
-            % Visualize Error
-            step(sa,[noisyData(timeIndex),offsetData(timeIndex)]);pause(0.1); %#ok<*UNRCH>
-        end
+    % Correct data with fine frequency compensator
+    [fineCorrectedData(timeIndex), correctedPhaseEstimate(timeIndex)] = ...
+        fineFreqComp(coarseCorrectedData(timeIndex));
+    rmsEVM(timeIndex) = evm(correctedPhaseEstimate(timeIndex));
 
-        if visualOffsetCorrected
-            % Visualize Corrected Data
-            step(sa,[noisyData(timeIndex),coarseCorrectedData(timeIndex)]);pause(0.1); %#ok<*UNRCH>
-        end
-
+    if visualOffset
+        % Visualize Error
+        step(sa,[noisyData(timeIndex),offsetData(timeIndex)]);pause(0.1); %#ok<*UNRCH>
     end
 
-    %% Section 2.1: Set up fig info to be saved
-    if plot_2_1
-        figPlot = figure;
-        figName = sprintf("MATLAB_2.1_2_offset_%d", offsetIncrease);
-        df = sampleRateHz/frameSize;
-        %% Plot
-        frequencies = -sampleRateHz/2:df:sampleRateHz/2-df;
-        spec = @(sig) fftshift(10*log10(abs(fft(sig))));
-        h = plot(frequencies, spec(noisyData(timeIndex)),...
-            frequencies, spec(offsetData(timeIndex)));
-        grid on;xlabel('Frequency (Hz)');ylabel('PSD (dB)');
-        legend('Original','Offset','Location','Best');
-        title(sprintf('PSDs of offset and original transmitted signals with offset = %d Hz', offsetIncrease));
-        NumTicks = 5;L = h(1).Parent.XLim;
-        set(h(1).Parent,'XTick',linspace(L(1),L(2),NumTicks))
-
-        %% Save figure
-        saveas(figPlot, strcat(figName, '.fig'));
-        saveas(figPlot, strcat(figName, '.jpg'));
+    if visualOffsetCorrected
+        % Visualize Corrected Data
+        step(sa,[coarseCorrectedData(timeIndex),fineCorrectedData(timeIndex)]);pause(0.1); %#ok<*UNRCH>
+        % Visualize Corrected Data
+        %step(sa,[noisyData(timeIndex),coarseCorrectedData(timeIndex)]);pause(0.1); %#ok<*UNRCH>
     end
-
-
-    %% Section 3: Set up fig info to be saved
-    if plot_3_2
-        figPlot = figure;
-        figName = sprintf("MATLAB_3.2_offset_%d_mod_%s", offsetIncrease, modType);
-        df = sampleRateHz/frameSize;
-        %% Plot
-        frequencies = -sampleRateHz/2:df:sampleRateHz/2-df;
-        spec = @(sig) fftshift(10*log10(abs(fft(sig))));
-        h = plot(frequencies, spec(noisyData(timeIndex)),...
-            frequencies, spec(coarseCorrectedData(timeIndex)));
-        grid on;xlabel('Frequency (Hz)');ylabel('PSD (dB)');
-        legend('Original','Coarse Corrected Offset','Location','Best');
-        title(sprintf('(%s) PSDs of coarse freq corrected offset and original tx signals with offset = %d Hz', modType, offsetIncrease));
-        NumTicks = 5;L = h(1).Parent.XLim;
-        set(h(1).Parent,'XTick',linspace(L(1),L(2),NumTicks))
-
-        %% Save figure
-        saveas(figPlot, strcat(figName, '.fig'));
-        saveas(figPlot, strcat(figName, '.jpg'));
-    end
-
-    rmsEVM = evm(correctedPhaseEstimate)
 
 end
+
+%% Section 2.1: Set up fig info to be saved
+if plot_2_1
+    figPlot = figure;
+    figName = sprintf("MATLAB_2.1_2_offset_%d", offsetIncrease);
+    df = sampleRateHz/frameSize;
+    %% Plot
+    frequencies = -sampleRateHz/2:df:sampleRateHz/2-df;
+    spec = @(sig) fftshift(10*log10(abs(fft(sig))));
+    h = plot(frequencies, spec(noisyData(timeIndex)),...
+        frequencies, spec(offsetData(timeIndex)));
+    grid on;xlabel('Frequency (Hz)');ylabel('PSD (dB)');
+    legend('Original','Offset','Location','Best');
+    title(sprintf('PSDs of offset and original transmitted signals with offset = %d Hz', offsetIncrease));
+    NumTicks = 5;L = h(1).Parent.XLim;
+    set(h(1).Parent,'XTick',linspace(L(1),L(2),NumTicks))
+
+    %% Save figure
+    saveas(figPlot, strcat(figName, '.fig'));
+    saveas(figPlot, strcat(figName, '.jpg'));
+end
+
+
+%% Section 3: Set up fig info to be saved
+if plot_3_2
+    figPlot = figure;
+    figName = sprintf("MATLAB_3.2_offset_%d_mod_%s", offsetIncrease, modType);
+    df = sampleRateHz/frameSize;
+    %% Plot
+    frequencies = -sampleRateHz/2:df:sampleRateHz/2-df;
+    spec = @(sig) fftshift(10*log10(abs(fft(sig))));
+    h = plot(frequencies, spec(noisyData(timeIndex)),...
+        frequencies, spec(coarseCorrectedData(timeIndex)));
+    grid on;xlabel('Frequency (Hz)');ylabel('PSD (dB)');
+    legend('Original','Coarse Corrected Offset','Location','Best');
+    title(sprintf('(%s) PSDs of coarse freq corrected offset and original tx signals with offset = %d Hz', modType, offsetIncrease));
+    NumTicks = 5;L = h(1).Parent.XLim;
+    set(h(1).Parent,'XTick',linspace(L(1),L(2),NumTicks))
+
+    %% Save figure
+    saveas(figPlot, strcat(figName, '.fig'));
+    saveas(figPlot, strcat(figName, '.jpg'));
+end
+
--- Lab_6/Source_Code/Yee_Taylor_ECE_531_lab6part2.m
+++ Lab_6/Source_Code/Yee_Taylor_ECE_531_lab6part2.m
@@ -1,7 +1,8 @@
 %% Lab 1 Part 2: Fine Frequency Correction
+clear all; close all;
 
 % Debugging flags
-visuals = true;
+visuals = false;
 
 %% General system details
 sampleRateHz = 1e3; % Sample rate
@@ -18,26 +19,33 @@ cdPost = comm.ConstellationDiagram('ReferenceConstellation', [-1 1],...
     'SymbolsToDisplaySource','Property',...
     'SymbolsToDisplay',frameSize/2,...
     'Name','Baseband with Freq Offset');
+cdCorrected = comm.ConstellationDiagram('ReferenceConstellation', [-1 1],...
+    'SymbolsToDisplaySource','Property',...
+    'SymbolsToDisplay',frameSize/2,...
+    'Name','Baseband with Freq Offset FFC Corrected');
 cdPre.Position(1) = 50;
 cdPost.Position(1) = cdPre.Position(1)+cdPre.Position(3)+10;% Place side by side
+cdCorrected.Position(1) = cdPre.Position(1)+cdPre.Position(3)+50;
 ap = dsp.ArrayPlot;ap.ShowGrid = true;
 ap.Title = 'Frequency Histogram';ap.XLabel = 'Hz';ap.YLabel = 'Magnitude';
 ap.XOffset = -sampleRateHz/2;
 ap.SampleIncrement = (sampleRateHz)/(2^10);
 
-cdOut = comm.ConstellationDiagram('ReferenceConstellation', [-1 1],...
-    'Name','Baseband');
-cdPreOut = comm.ConstellationDiagram('ReferenceConstellation', [-1 1],...
-    'Name','Baseband');
-
 %% Impairments
 snr = 15;
 frequencyOffsetHz = sampleRateHz*0.02; % Offset in hertz
-phaseOffset = 0; % Radians
+phaseOffset = pi/8; % Radians
 
 %% Generate symbols
 data = randi([0 samplesPerSymbol], numSamples, 1);
 modulatedData = mod.step(data);
+
+%% Setup Fine Frequency Compensator
+fineFreqComp = comm.CarrierSynchronizer('Modulation','BPSK', ...
+    'SamplesPerSymbol',samplesPerSymbol, 'CustomPhaseOffset',phaseOffset);
+
+%% Add error vector magnitude (EVM)
+evm = comm.EVM('ReferenceSignalSource','Estimated from reference constellation');
 
 %% Add noise
 noisyData = awgn(modulatedData,snr);%,'measured');
@@ -48,7 +56,10 @@ noisyData = awgn(modulatedData,snr);%,'measured');
 % Precalculate constants
 normalizedOffset = 1i.*2*pi*frequencyOffsetHz./sampleRateHz;
 
-offsetData = zeros(size(noisyData));
+offsetData    = zeros(size(noisyData));
+fineCorrectedData = zeros(size(noisyData));
+correctedPhaseEstimate = zeros(size(noisyData));
+
 for k=1:frameSize:numSamples
     
     timeIndex = (k:k+frameSize-1).';
@@ -56,10 +67,23 @@ for k=1:frameSize:numSamples
     
     % Offset data and maintain phase between frames
     offsetData(timeIndex) = noisyData(timeIndex).*freqShift;
+
+    % Next, correct offset data with fine frequency compensator
+    [fineCorrectedData(timeIndex), correctedPhaseEstimate(timeIndex)] = fineFreqComp(offsetData(timeIndex));
     
     % Visualize Error
     if visuals
-        step(cdPre,noisyData(timeIndex));step(cdPost,offsetData(timeIndex));pause(0.1); %#ok<*UNRCH>
+        % Compare noisy data to offset data
+        %step(cdPre,noisyData(timeIndex));step(cdPost,offsetData(timeIndex));pause(0.1); %#ok<*UNRCH>
+
+        % Compare corrected data to offset data
+        step(cdPre,noisyData(timeIndex));step(cdCorrected,fineCorrectedData(timeIndex));step(cdPost,offsetData(timeIndex));pause(0.1); %#ok<*UNRCH>
+
+        % Compare noisy data to corrected data
+        %step(cdPre,noisyData(timeIndex));step(cdPost,correctedData(timeIndex));pause(0.1); %#ok<*UNRCH>
+
     end
     
 end
+
+rmsEVM = evm(correctedPhaseEstimate)
